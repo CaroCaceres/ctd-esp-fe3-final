e6c686a82d79891ea841cff0d3c2528cd736679f
CaroCaceres <carolinacaceresp10@gmail.com>
2023-04-03 06:26:18 -0500

    Implementando routes y ContextGlobal

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
+import React, { useContext } from "react";
+import { Route, Routes } from "react-router-dom";
 
+// Routes
+import { routes } from "./Routes/routes";
+import Contact from "./Routes/Contact";
+import Detail from "./Routes/Detail";
+import Favs from "./Routes/Favs";
+import Home from "./Routes/Home";
+
+// Components
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+
+// Context
+import { ContextProvider } from "./Components/utils/global.context";
 
 
 function App() {
   return (
-      <div className="App">
-          <Navbar/>
-          <Footer/>
-      </div>
+    <div className="App">
+      <ContextProvider children={<Navbar />} />
+      <Routes>
+        <Route path={routes.home} element={
+          <ContextProvider children={<Home />} />
+        } />
+        <Route path={routes.dentist} element={
+          <ContextProvider children={<Detail />} />
+        } />
+        <Route path={routes.contact} element={
+          <ContextProvider children={<Contact />} />
+        } />
+        <Route path={routes.favs} element={
+          <ContextProvider children={<Favs />} />
+        } />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
