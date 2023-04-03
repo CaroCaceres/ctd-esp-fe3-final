1785c721ebd832b901b49c848bd20e3e26be3e43
CaroCaceres <carolinacaceresp10@gmail.com>
2023-04-03 06:32:58 -0500

    Implementando Favs

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Card from "../Components/Card";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Favs = () => {
+  const favsDentists = JSON.parse(localStorage.getItem("favsDentists")) || [];
 
   return (
     <>
       <h1>Dentists Favs</h1>
       <div className="card-grid">
-        {/* este componente debe consumir los destacados del localStorage */}
-        {/* Deberan renderizar una Card por cada uno de ellos */}
+        {/* este componente debe consumir los destacados del localStorage ✅ */}
+        {/* Deberan renderizar una Card por cada uno de ellos ✅ */}
+        {favsDentists.map((user) => (
+          <Card id={user.id} name={user.name} username={user.username} key={user.id} />
+        ))}
       </div>
     </>
   );
