f81b4baff6661c76f6f30a1cf618f3c2696838d6
CaroCaceres <carolinacaceresp10@gmail.com>
2023-04-03 06:30:34 -0500

    Implementando Home y Card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,20 +1,81 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
+import { Link } from "react-router-dom";
+
+// Styles
+import "./Card.css";
+
+// Utils
+const uuidGenerator = ({ name, username, id }) => {
+  return `${username}-${id}-${name}`;
+}
+
 
 
 const Card = ({ name, username, id }) => {
-
-  const addFav = ()=>{
-    // Aqui iria la logica para agregar la Card en el localStorage
+  const addFav = (state, action) => {
+    // Aqui iria la logica para agregar la Card en el localStorage ✅
+    const dentists = JSON.parse(localStorage.getItem("favsDentists")) || [];
+    const username = dentists.find(
+      (dentist) => dentist.username === action.payload.username
+    );
+    switch (action.type) {
+      case "CHECK_FAV":
+        if (username) {
+          return { added: true };
+        }
+        return { added: false };
+      case "ADD_FAV":
+        dentists.push(action.payload);
+        localStorage.setItem(
+          "favsDentists",
+          JSON.stringify(dentists)
+        );
+        alert(`username: ${action.payload.username} added to favorites`);
+        return { added: !state.added };
+      case "REMOVE_FAV":
+        dentists.splice(dentists.indexOf(action.payload), 1);
+        localStorage.setItem(
+          "favsDentists",
+          JSON.stringify(dentists)
+        )
+        alert(`username: ${action.payload.username} removed from favorites`);
+        return { added: !state.added };
+      default:
+        throw new Error();
+    }
   }
+
+  const [fav, dispatch] = useReducer(
+    addFav,
+    {
+      added: false
+    }
+  )
+
+  useEffect(() => {
+    dispatch({ type: "CHECK_FAV", payload: { name, username, id } });
+  }, []);
 
   return (
     <div className="card">
-        {/* En cada card deberan mostrar en name - username y el id */}
+      {/* En cada card deberan mostrar en name - username y el id ✅ */}
+      {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle ✅ */}
+      <img src={"/images/doctor.jpg"} alt="dentist" id="doctor" />
+      <Link to={`/dentist/${id}`}>
+        <h3>{name}</h3>
+      </Link>
+      <p>{username}</p>
 
-        {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
-
-        {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
-        <button onClick={addFav} className="favButton">Add fav</button>
+      {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage ✅ */}
+      <button onClick={() => {
+        if (fav.added) {
+          dispatch({ type: "REMOVE_FAV", payload: { id, name, username } });
+        } else {
+          dispatch({ type: "ADD_FAV", payload: { id, name, username } });
+        }
+        // Recarga la pagina cada vez que se agrega o elimina un favorito
+        window.location.reload();
+      }} className="favButton">{ fav.added === true ? '⭐️' : '☆' }</button>
     </div>
   );
 };
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import { useContext, useEffect } from 'react'
 import Card from '../Components/Card'
+import { ContextGlobal } from '../Components/utils/global.context'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Home = () => {
+  const { dataProvider } = useContext(ContextGlobal);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => dataProvider.setData(data))
+  }, []);
+
   return (
     <main className="" >
       <h1>Home</h1>
       <div className='card-grid'>
-        {/* Aqui deberias renderizar las cards */}
+        {/* Aqui deberias renderizar las cards ✅ */
+          dataProvider.data.map((user) => (
+            <Card id={user.id} name={user.name} username={user.username} key={user.id}/>
+          ))
+        }
       </div>
     </main>
   )
