c2da751970ca928e0de15e39ba514c6871bf1901
CaroCaceres <carolinacaceresp10@gmail.com>
2023-04-03 06:31:35 -0500

    Implementando Detail

--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
-const Detail = () => {
- 
-  // Consumiendo el parametro dinamico de la URL deberan hacer un fetch a un user en especifico
+const Detail = (props) => {
+  // Consumiendo el parametro dinamico de la URL deberan hacer un fetch a un user en especifico ✅
+  const params = useParams()
+
+  const [dentist, setDentist] = useState({})
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)
+      .then(response => response.json())
+      .then(data => setDentist(data))
+  }, []);
 
   return (
     <>
-      <h1>Detail Dentist id </h1>
-      {/* aqui deberan renderizar la informacion en detalle de un user en especifico */}
-      {/* Deberan mostrar el name - email - phone - website por cada user en especifico */}
+      <h1>Detail Dentist {params.id} </h1>
+      {/* aqui deberan renderizar la informacion en detalle de un user en especifico ✅ */}
+      {/* Deberan mostrar el name - email - phone - website por cada user en especifico ✅ */}
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Website</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{dentist.name}</td>
+            <td>{dentist.email}</td>
+            <td>{dentist.phone}</td>
+            <td>{dentist.website}</td>
+          </tr>
+        </tbody>
+      </table>
     </>
   )
 }
