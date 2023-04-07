6c423199d2663c61786231108a2e261c03664d2a
CaroCaceres <carolinacaceresp10@gmail.com>
2023-04-07 18:32:41 -0500

    style: Implementando modo dark y estilos - Contact

--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import Form from '../Components/Form'
+import React, {useContext} from 'react';
 
-//Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
+// Components
+import Form from '../Components/Form';
 
-const Contact = () => {
+// Context
+import {ContextGlobal} from '../Components/utils/global.context';
+
+// Styles
+import './Contact.css';
+
+function Contact() {
+  const {themeProvider} = useContext(ContextGlobal);
+
   return (
-    <div>
-      <h2>Want to know more?</h2>
-      <p>Send us your questions and we will contact you</p>
-      <Form/>
+    <div className={`contact-main ${themeProvider.theme.value}`}>
+      <div>
+        <h2>Want to know more?</h2>
+        <p>Send us your questions and we will contact you</p>
+      </div>
+      <Form />
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
