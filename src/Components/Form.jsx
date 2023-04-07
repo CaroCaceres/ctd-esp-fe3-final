ea8e6ac86f93fd38aac51c5b779e2529f0901a20
CaroCaceres <carolinacaceresp10@gmail.com>
2023-04-07 18:30:52 -0500

    style: Implementando modo dark y estilos - Form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,15 +1,13 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-
-const Form =  () => {
-  //Aqui deberan implementar el form completo con sus validaciones
+function Form() {
   const [form, setForm] = useState({
-    name: "",
-    email: "",
+    name: '',
+    email: '',
   });
 
   const [errors, setErrors] = useState({
-    message: "Por favor verifique su información nuevamente",
+    message: 'Por favor verifique su información nuevamente',
     show: false,
   });
 
@@ -40,21 +38,18 @@ const Form =  () => {
       show: false,
     });
     setSuccess(true);
-    console.log(form);
   };
 
   return (
-    <div>
+    <>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" onChange={handleChange}/>
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" onChange={handleChange}/>
+          <input type="text" name="name" id="name" placeholder="Name" onChange={handleChange} />
+          <input type="email" name="email" id="email" placeholder="Email" onChange={handleChange} />
         <button type="submit">Send</button>
       </form>
       {errors.show && <p>{errors.message}</p>}
       {success && <p>Gracias {form.name}, te contactaremos cuando antes vía mail</p>}
-    </div>
+    </>
   );
 }
 
