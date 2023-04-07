73b1b67e790af60b0937236a246373964e4d800f
CaroCaceres <carolinacaceresp10@gmail.com>
2023-04-07 18:30:08 -0500

    style: Implementando modo dark y estilos - App

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,33 @@
-import React, { useContext } from "react";
-import { Route, Routes } from "react-router-dom";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 // Routes
-import { routes } from "./Routes/routes";
-import Contact from "./Routes/Contact";
-import Detail from "./Routes/Detail";
-import Favs from "./Routes/Favs";
-import Home from "./Routes/Home";
+import routes from './Routes/routes';
+import Contact from './Routes/Contact';
+import Detail from './Routes/Detail';
+import Favs from './Routes/Favs';
+import Home from './Routes/Home';
 
 // Components
-import Footer from "./Components/Footer";
-import Navbar from "./Components/Navbar";
+import Footer from './Components/Footer';
+import Navbar from './Components/Navbar';
 
-// Context
-import { ContextProvider } from "./Components/utils/global.context";
-
+// Styles
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <ContextProvider children={<Navbar />} />
+    <>
+      <Navbar />
       <Routes>
-        <Route path={routes.home} element={
-          <ContextProvider children={<Home />} />
-        } />
-        <Route path={routes.dentist} element={
-          <ContextProvider children={<Detail />} />
-        } />
-        <Route path={routes.contact} element={
-          <ContextProvider children={<Contact />} />
-        } />
-        <Route path={routes.favs} element={
-          <ContextProvider children={<Favs />} />
-        } />
+        <Route path={routes.home} element={<Home />} />
+        <Route path={routes.dentist} element={<Detail />} />
+        <Route path={routes.contact} element={<Contact />} />
+        <Route path={routes.favs} element={<Favs />} />
         <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
       <Footer />
-    </div>
+    </>
   );
 }
 
