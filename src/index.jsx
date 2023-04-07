ae62c727edacbc58b85ae7ed722838b56e486485
CaroCaceres <carolinacaceresp10@gmail.com>
2023-04-07 18:29:25 -0500

    Moviendo ContextProvider a un nivel más alto

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+// Styles
+import './index.css';
+
+// Components
+import App from './App';
+
+// Context
+import { ContextProvider } from './Components/utils/global.context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-      <App/>
-  </BrowserRouter>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </BrowserRouter>,
 );
-
-
