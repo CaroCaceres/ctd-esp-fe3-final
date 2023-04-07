79be3d49664fdb924af8b60163bdf1bb4f1ee2b8
CaroCaceres <carolinacaceresp10@gmail.com>
2023-04-07 18:30:31 -0500

    style: Implementando modo dark y estilos - Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, {useContext} from 'react';
 
-const Footer = () => {
+// Context
+import {ContextGlobal} from './utils/global.context';
+
+// Styles
+import './Footer.css';
+
+function Footer() {
+  const { themeProvider } = useContext(ContextGlobal);
+
   return (
-    <footer>
-        <p>Powered by</p>
-        <img src="./images/DH.png" alt='DH-logo' />
+    <footer className={themeProvider.theme.value}>
+      <p>Powered by</p>
+      <img src="./images/DH.png" alt="DH-logo" />
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
